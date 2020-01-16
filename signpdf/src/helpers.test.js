73b7469faa3b7cd5778055d861b532da637d2d3e
Valery Buchinsky <v.buchinsky@gaida.ai>
2020-01-16 13:34:11 +0200

    So no pdfkit. Will have to create those resources.

--- signpdf/src/helpers.test.js
+++ signpdf/src/helpers.test.js
@@ -1,58 +1,7 @@
 import fs from 'fs';
-import PDFDocument from 'pdfkit';
 import signer from './signpdf';
-import {pdfkitAddPlaceholder, extractSignature} from './helpers';
+import {extractSignature} from './helpers';
 import SignPdfError from './SignPdfError';
-
-/**
- * Creates a Buffer containing a PDF.
- * Returns a Promise that is resolved with the resulting Buffer of the PDFDocument.
- * @returns {Promise<Buffer>}
- */
-const createPdf = (params = {
-    placeholder: {},
-    text: 'node-signpdf',
-}) => new Promise((resolve) => {
-    const pdf = new PDFDocument({
-        autoFirstPage: true,
-        size: 'A4',
-        layout: 'portrait',
-        bufferPages: true,
-    });
-    pdf.info.CreationDate = '';
-
-    // Add some content to the page
-    pdf
-        .fillColor('#333')
-        .fontSize(25)
-        .moveDown()
-        .text(params.text);
-
-    // Collect the ouput PDF
-    // and, when done, resolve with it stored in a Buffer
-    const pdfChunks = [];
-    pdf.on('data', (data) => {
-        pdfChunks.push(data);
-    });
-    pdf.on('end', () => {
-        resolve(Buffer.concat(pdfChunks));
-    });
-
-    // Externally (to PDFKit) add the signature placeholder.
-    const refs = pdfkitAddPlaceholder({
-        pdf,
-        pdfBuffer: Buffer.from([pdf]),
-        reason: 'I am the author',
-        ...params.placeholder,
-    });
-    // Externally end the streams of the created objects.
-    // PDFKit doesn't know much about them, so it won't .end() them.
-    Object.keys(refs).forEach(key => refs[key].end());
-
-    // Also end the PDFDocument stream.
-    // See pdf.on('end'... on how it is then converted to Buffer.
-    pdf.end();
-});
 
 describe('Helpers', () => {
     it('extract signature from signed pdf', async () => {
--- signpdf/src/signpdf.test.js
+++ signpdf/src/signpdf.test.js
@@ -1,65 +1,8 @@
-import PDFDocument from 'pdfkit';
 import forge from 'node-forge';
 import fs from 'fs';
 import signer from './signpdf';
-import {pdfkitAddPlaceholder, extractSignature, plainAddPlaceholder} from './helpers';
+import {extractSignature} from './helpers';
 import SignPdfError from './SignPdfError';
-
-/**
- * Creates a Buffer containing a PDF.
- * Returns a Promise that is resolved with the resulting Buffer of the PDFDocument.
- * @returns {Promise<Buffer>}
- */
-const createPdf = params => new Promise((resolve) => {
-    const requestParams = {
-        placeholder: {},
-        text: 'node-signpdf',
-        addSignaturePlaceholder: true,
-        ...params,
-    };
-
-    const pdf = new PDFDocument({
-        autoFirstPage: true,
-        size: 'A4',
-        layout: 'portrait',
-        bufferPages: true,
-    });
-    pdf.info.CreationDate = '';
-
-    // Add some content to the page
-    pdf
-        .fillColor('#333')
-        .fontSize(25)
-        .moveDown()
-        .text(requestParams.text);
-
-    // Collect the ouput PDF
-    // and, when done, resolve with it stored in a Buffer
-    const pdfChunks = [];
-    pdf.on('data', (data) => {
-        pdfChunks.push(data);
-    });
-    pdf.on('end', () => {
-        resolve(Buffer.concat(pdfChunks));
-    });
-
-    if (requestParams.addSignaturePlaceholder) {
-        // Externally (to PDFKit) add the signature placeholder.
-        const refs = pdfkitAddPlaceholder({
-            pdf,
-            pdfBuffer: Buffer.from([pdf]),
-            reason: 'I am the author',
-            ...requestParams.placeholder,
-        });
-        // Externally end the streams of the created objects.
-        // PDFKit doesn't know much about them, so it won't .end() them.
-        Object.keys(refs).forEach(key => refs[key].end());
-    }
-
-    // Also end the PDFDocument stream.
-    // See pdf.on('end'... on how it is then converted to Buffer.
-    pdf.end();
-});
 
 describe('Test signing', () => {
     it('expects PDF to be Buffer', () => {
